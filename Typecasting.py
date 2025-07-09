dd4a5786caf6a6ce4a07c15cf6bbd0771d55fec0
IAMDAGOOAT <gopinathanaar@gmail.com>
2025-07-09 12:43:50 +0800

    Typecasting

---
+++ Typecasting.py
@@ -0,0 +1,16 @@
+name="Taj"
+print("name :", name)
+print("data types of name is", type(name))
+age=9
+print("age :", age)
+print("data types of age is", type(age))
+weight=13.5
+print("weight :", weight)
+print("data types of weight is", type(weight))
+Hi=True
+print("Hi :", Hi)
+print("data types of Hi is", type(Hi))
+print("\n After Typing Casting....")
+weight = int(weight)
+print(weight)
+print("data types of weight is", type(weight))
