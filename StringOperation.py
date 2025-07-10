4764cc9abc3147b2bb6693c2317bb60ec888fe71
IAMDAGOOAT <gopinathanaar@gmail.com>
2025-07-10 12:22:34 +0800

    String Operations

--- StringOperation.py
+++ StringOperation.py
@@ -1,9 +1,13 @@
 str1 = "6"
 str2 = "9"
-
 print('str1 + str2 = ', str1 + str2)
 a = 1
 b = 2
 print('a + b is', a + b)
 Str = "ByBy"
 print(Str[::-2])
+text = (input("Enter A String: "))
+
+revText = text[:: -1]
+text = revText
+print(text)
